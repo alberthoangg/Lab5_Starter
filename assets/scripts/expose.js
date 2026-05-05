614e27e5a343034905201ac8322fc8ff3084bef9
Fresh-Bruh <noahterrell84@gmail.com>
2026-05-04 22:24:54 -0700

    fixed js file error
    expose js was put into wrong js file but is not in the correct file.

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,66 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+    const confetti = new JSConfetti()
+   
+    let volumeSlider = document.getElementById("volume")
+
+    let audio = document.querySelector("audio");
+
+    let soundButton = document.querySelector("button");
+    
+    let hornImg = document.querySelector("img");
+
+    let dropDown = document.getElementById("horn-select");
+
+    let soundIcon = document.querySelector("#volume-controls img");
+        
+    
+
+    dropDown.addEventListener('change', function(){
+        hornImg.src = "assets/images/" + dropDown.value + ".svg";
+        hornImg.alt = dropDown.value + " image";
+        audio.src = "assets/audio/" + dropDown.value + ".mp3";
+        // console.log(hornImg.alt)
+        // console.log(audio)
+        // console.log(dropDown.value)
+    });
+
+    soundButton.addEventListener('click', function(){
+
+        if (dropDown.value === "select"){
+            return;
+        } 
+        audio.play();
+
+        if (dropDown.value === "party-horn"){
+            confetti.addConfetti();
+        }
+        
+        
+    });
+    
+    volumeSlider.addEventListener('input', function(){
+        let volumeLevel = volumeSlider.value;
+        // let soundIcon = document.querySelectorAll("img")[1];
+        
+        if (volumeLevel == 0){
+            soundIcon.src = "assets/icons/volume-level-0.svg";
+            soundIcon.alt = "Volume level 0";
+        } else if (volumeLevel < 33){
+            soundIcon.src = "assets/icons/volume-level-1.svg";
+            soundIcon.alt = "Volume level 1";
+        } else if (volumeLevel < 67){
+            soundIcon.src = "assets/icons/volume-level-2.svg";
+            soundIcon.alt = "Volume level 2";
+        } else{
+            soundIcon.src = "assets/icons/volume-level-3.svg";
+            soundIcon.alt = "Volume level 3";
+        }
+        audio.volume = (Number(volumeLevel)/100);
+    })
+        
+
 }
+
+
